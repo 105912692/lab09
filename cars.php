fe213b5dda930a6947e8e52e5c8788e44c895373
Oliver Scott <105912692@student.swin.edu.au>
2025-05-05 17:44:28 +1000

    cars.php task 6 attempt in progress

---
+++ cars.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "settings.php";
+    $dbconn = @mysqli_connect($host,$user,$pwd,$sql_db);
+    if($dbconn)
+        $query = "SELECT * FROM cars";
+        $result = mysqli_query ($dbconn, $query)
+        if($result){...}
+        else{...}
+        mysqli_close($dbconn);
+    else echo "<p>Unable to connect to db</p>";
+?>
