727bd026e05c86ddfba542390057086ffc91a5cb
Oliver Scott <105912692@student.swin.edu.au>
2025-05-05 17:47:02 +1000

    task 7 cars.php attempt progress

--- cars.php
+++ cars.php
@@ -4,8 +4,20 @@
     if($dbconn)
         $query = "SELECT * FROM cars";
         $result = mysqli_query ($dbconn, $query)
-        if($result){...}
-        else{...}
+        if($result){
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+                }
+        else{
+            echo "<p>There are noi cars to display.</p>"
+        }
         mysqli_close($dbconn);
     else echo "<p>Unable to connect to db</p>";
 ?>
